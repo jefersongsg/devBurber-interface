f9c94c61930c80b8ea330727e319433ac14262ee
jefersongsg <jefersonsilva5525@gmail.com>
2025-01-09 15:25:50 -0300

    corrigindo erros na verificaçao dos erros

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -11,36 +11,41 @@ import { api } from '../../services/api';
 
 export function Login() {
 
-    const schema = yup
-  .object({
-    email: yup.string().email().required(),
-    password: yup.string().min(6).required(),
-  })
-  .required();
+    const schema = yup.object({
+        email: yup
+            .string()
+            .email('Digite um e-mail válido')
+            .required('O e-mail é obrigatório'),
+        password: yup
+            .string()
+            .min(6, 'A senha deve ter pelo menos 6 caracteres')
+            .required('Digite um senha'),
+    })
+        .required();
 
     const {
         register,
         handleSubmit,
         formState: { errors },
-      } = useForm({
+    } = useForm({
         resolver: yupResolver(schema),
-      })
-      const onSubmit = async (data) => {
-       const response = 
-       await toast.promise(
-        api.post('/session',{
-            email: data.email,
-            password: data.password,
-        }),
-        {
-            pending: 'Verificando seus dados',
-            success: 'Seja Bem-vindo(a)',
-            error: 'Email ou senha Incorretos',
-        },
-       );
+    })
+    const onSubmit = async (data) => {
+        const response =
+            await toast.promise(
+                api.post('/session', {
+                    email: data.email,
+                    password: data.password,
+                }),
+                {
+                    pending: 'Verificando seus dados',
+                    success: 'Seja Bem-vindo(a)',
+                    error: 'Email ou senha Incorretos',
+                },
+            );
 
         console.log(response)
-      };
+    };
 
     return (
         <Container>
@@ -50,19 +55,19 @@ export function Login() {
             <RightContainer>
                 <Title>
                     Olá, seja bem vindo ao <span>Dev Burguer!</span>
-                    <br/>
+                    <br />
                     Acesse com seu <span>Login e senha.</span>
                 </Title>
                 <Form onSubmit={handleSubmit(onSubmit)}>
                     <InputContainer>
                         <label>Email</label>
-                        <input type='email' {...register('email')}/>
+                        <input type='email' {...register('email')} />
                         <p>{errors?.email?.message}</p>
                     </InputContainer>
 
                     <InputContainer>
                         <label>Senha</label>
-                        <input type='password' {...register('password')}/>
+                        <input type='password' {...register('password')} />
                         <p>{errors?.password?.message}</p>
                     </InputContainer>
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,16 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { Login } from "../containers/Login";
+import { Register } from "../containers/Register";
 
 export const router = createBrowserRouter([
     {
-        path: '/',
-        element: <Login/>
+        path: '/login',
+        element: <Login/>,
+    },
+    {
+        path: '/cadastro',
+        element: <Register/>,
     },
     
-])
+]);
