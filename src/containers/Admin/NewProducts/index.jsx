81580e59bef6c4b1da942e0c312e83076350c13c
jefersongsg <jefersonsilva5525@gmail.com>
2025-02-01 15:52:11 -0300

    pagina das ordens ok criando pagina de novo produto stylos pronto so configurar com backend

--- src/containers/Admin/NewProducts/index.jsx
+++ src/containers/Admin/NewProducts/index.jsx
@@ -1,8 +1,56 @@
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Image } from '@phosphor-icons/react';
+import { Container, Form, InputGrup, Label, Input, LabelUpload, Select, SubmitButt } from './styles';
+
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+  })
+  .required()
+
 export function NewProducts() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({
+        resolver: yupResolver(schema),
+      })
+      const onSubmit = (data) => console.log(data);
+
     return(
-        <div>
-            <h1>Novos Produtos</h1>
-        </div>
+        <Container>
+            <Form>
+                <InputGrup>
+                <Label>Nome</Label>
+                <Input/>
+                </InputGrup>
+
+                <InputGrup>
+                <Label>Preço</Label>
+                <Input type='number'
+                min={5}/>
+                </InputGrup>
+
+                <InputGrup>
+                <LabelUpload>
+                <Image/> Carregar imagem do produto
+                <input type="file"/>
+                </LabelUpload>
+                </InputGrup>
+
+                <InputGrup>
+                <Label>Categoria</Label>
+                <Select/>
+                </InputGrup>
+
+                <SubmitButt>Adicionar Produto</SubmitButt>
+            </Form>
+        </Container>
     )
 }
---
+++ src/containers/Admin/NewProducts/styles.js
@@ -0,0 +1,68 @@
+
+import { Button } from '../../../components';
+
+import ReactSelect from 'react-select';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80vh;
+`;
+
+export const Form = styled.form`
+    border-radius: 10px;
+    background-color: ${(props) => props.theme.black};
+    padding: 35px;
+    width: 100%;
+    max-width: 380px;
+    display: flex;
+    flex-direction: column;
+    gap: 29px;
+`;
+
+export const InputGrup = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 4px; 
+`;
+
+export const Label = styled.label`
+   color: ${(props) => props.theme.white};
+   font-size: 16px;
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    height: 48px;
+    border-radius: 5px;
+    padding: 0 12px;
+    border: none;
+`;
+
+export const LabelUpload = styled.label`
+    cursor: pointer;
+    border: 1px dashed ${(props) => props.theme.white};
+    border-radius: 5px;
+    padding: 10px;
+    display: flex;
+    color: ${(props) => props.theme.white};
+    margin: 20px 0; 
+
+    input{
+        display: none;
+    }
+    > svg {
+        width: 20px;
+        height: 20px;
+        fill: ${(props) => props.theme.white};
+        margin-right: 10px;
+    }
+`;
+
+export const Select = styled(ReactSelect)``;
+
+export const SubmitButt = styled(Button)`
+    margin-top: 40px;
+`;
