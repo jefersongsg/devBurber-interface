d64dbbe8e86b2f8ee22640fbf55f515624f9b4ce
jefersongsg <jefersonsilva5525@gmail.com>
2025-02-03 15:57:09 -0300

    pagina de criaçao de novo produto cnectado com backend tudo ok

--- src/containers/Admin/NewProducts/index.jsx
+++ src/containers/Admin/NewProducts/index.jsx
@@ -1,56 +1,125 @@
 
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { Image } from '@phosphor-icons/react';
-import { Container, Form, InputGrup, Label, Input, LabelUpload, Select, SubmitButt } from './styles';
+import { Container, Form, InputGrup, Label, Input, LabelUpload, Select, SubmitButt, ErrorMessage } from './styles';
+import { useEffect, useState } from 'react';
+import { api } from '../../../services/api';
+import { toast } from 'react-toastify';
 
 
 const schema = yup
   .object({
-    firstName: yup.string().required(),
-    age: yup.number().positive().integer().required(),
+    name: yup.string().required('Digite o nome do produto'),
+    price: yup.number().positive().required('Digite o preço do produto').typeError('Digite o preço do produto'),
+    category: yup.object().required('Escolha uma categoria'),
+    file: yup
+      .mixed()
+      .test('required', 'Escolha um arquivo para continuar', (value) => {
+        return value && value.length > 0;
+      })
+      .test('fileSize', 'Carregue arquivos até 3mb', (value) => {
+        return value && value.length > 0 && value[0].size <= 300000;
+      })
   })
   .required()
 
 export function NewProducts() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-      } = useForm({
-        resolver: yupResolver(schema),
-      })
-      const onSubmit = (data) => console.log(data);
+  const [filename, setFileName] = useState(null);
+  const [categories, setCategories] = useState([]);
 
-    return(
-        <Container>
-            <Form>
-                <InputGrup>
-                <Label>Nome</Label>
-                <Input/>
-                </InputGrup>
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
 
-                <InputGrup>
-                <Label>Preço</Label>
-                <Input type='number'
-                min={5}/>
-                </InputGrup>
+      setCategories(data);
 
-                <InputGrup>
-                <LabelUpload>
-                <Image/> Carregar imagem do produto
-                <input type="file"/>
-                </LabelUpload>
-                </InputGrup>
+    }
+    loadCategories();
+  }, [])
 
-                <InputGrup>
-                <Label>Categoria</Label>
-                <Select/>
-                </InputGrup>
 
-                <SubmitButt>Adicionar Produto</SubmitButt>
-            </Form>
-        </Container>
-    )
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = async (data) => {
+    const productFormData = new FormData();
+
+    productFormData.append('name', data.name);
+    productFormData.append('price', data.price * 100);
+    productFormData.append('category_id', data.category.id);
+    productFormData.append('file', data.file[0]);
+
+    await toast.promise(api.post('/products', productFormData), {
+      pending: 'Adicinando o produto...',
+      success: 'Produto criado com sucesso',
+      error: 'Falha ao adicionar o produto, tente novamente',
+     });
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <InputGrup>
+          <Label>Nome</Label>
+          <Input type='text'
+            autoComplete='none'
+            {...register('name')} />
+          <ErrorMessage>{errors?.name?.message}</ErrorMessage>
+        </InputGrup>
+
+        <InputGrup>
+          <Label>Preço</Label>
+          <Input type='number'
+            {...register('price')}
+            min={5} />
+          <ErrorMessage>{errors?.price?.message}</ErrorMessage>
+        </InputGrup>
+
+        <InputGrup>
+          <LabelUpload>
+            <Image />
+            <input type='file'
+              {...register('file')}
+              accept='image/png, image/jpeg'
+              onChange={(value) => {
+
+                setFileName(value.target.files[0]?.name);
+                register('file').onChange(value);
+              }}
+            />
+            {filename || 'Upload do produto'}
+          </LabelUpload>
+          <ErrorMessage>{errors?.file?.message}</ErrorMessage>
+        </InputGrup>
+
+        <InputGrup>
+          <Label>Categoria</Label>
+          <Controller
+            name='category'
+            control={control}
+            render={({ field }) => (
+              <Select
+                {...field}
+                options={categories}
+                getOptionLabel={(category) => category.name}
+                getOptionValue={(category) => category.id}
+                placeholder='Categorias'
+                menuPortalTarget={document.body}
+              />
+            )}
+          />
+          <ErrorMessage>{errors?.category?.message}</ErrorMessage>
+        </InputGrup>
+
+        <SubmitButt>Adicionar Produto</SubmitButt>
+      </Form>
+    </Container>
+  )
 }
--- src/containers/Admin/NewProducts/styles.js
+++ src/containers/Admin/NewProducts/styles.js
@@ -66,3 +66,10 @@ export const Select = styled(ReactSelect)``;
 export const SubmitButt = styled(Button)`
     margin-top: 40px;
 `;
+
+export const ErrorMessage = styled.span`
+     color: ${(props) => props.theme.darkRed};
+     font-size: 14px;
+     line-height: 80%;
+     font-weight: 60px;
+`;
